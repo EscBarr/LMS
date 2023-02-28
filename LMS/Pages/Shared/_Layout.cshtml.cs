d814d96dc65be873ccc8fd13fa7a52063ee5ad26
EscBar <timuryyaga@gmail.com>
2023-02-28 21:46:13 +0400

    Final Fixes

--- LMS/Pages/Shared/_Layout.cshtml.cs
+++ LMS/Pages/Shared/_Layout.cshtml.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ALS.Pages
+namespace LMS.Pages
 {
     public class _Layout : PageModel
     {
         public void OnGet()
         {
-            
         }
     }
 }
